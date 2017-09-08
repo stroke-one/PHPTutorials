d8d4355433bdce95f037d09a796b08d101c80f1e
stroke-one <stroke-one@users.noreply.github.com>
2017-09-08 10:31:43 -0700

    quick update to aws

--- index.php
+++ index.php
@@ -5,5 +5,12 @@
         </title>
     </head>
     <body>
-        <?php echo 'Hello World'; ?>
+        <form action="index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+        </form>
+        <?php
+            echo $_GET['name'];
+        ?>        
     </body>
