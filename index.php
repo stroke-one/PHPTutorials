683ffd31d1a1ac490629f88cf804ab175b14ffbf
stroke-one <stroke-one@users.noreply.github.com>
2017-09-08 11:07:04 -0700

    update

--- index.php
+++ index.php
@@ -5,12 +5,14 @@
         </title>
     </head>
     <body>
+    <center>
         <form action="index.php" method="post">
         Name: <input type="text" name="name"><br>
         E-mail: <input type="text" name="email"><br>
         <input type="submit">
         </form>
         <?php
-            echo $_GET['name'];
-        ?>        
+            echo $_POST['name'];
+        ?>
+    </center>        
     </body>
