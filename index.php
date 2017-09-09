4d34481ab59a23af2bccd06578990684ed422955
stroke-one <stroke-one@users.noreply.github.com>
2017-09-09 07:29:49 -0700

    mysql update

--- index.php
+++ index.php
@@ -12,7 +12,23 @@
         </form>
         <?php
             if (!empty($_POST['name'])){
-            echo $_POST['name'];
+                echo $_POST['name'];
+                $con = mysql_connect('localhost', 'ben', '...')
+                        or die('Unable to connect: ' . mysql_error());
+                mysql_select_db('phptest') or die ('unable to select db');
+                $sql_insert = 'insert into names (name) VALUES (' . $_POST['name'] . ')';
+                if (($con->query($sql) === TRUE){
+                    $sql = 'select name from names';
+                    $result = $con->query($sql);
+                    if ($result->num_rows > 0){
+                        while($row = $result->fetch_assoc())){
+                            echo("<p>" . $row["name"]) . "</p>"
+                        }
+                    }
+                    
+                     
+                }
+                
             }
         ?>
     </center>        
