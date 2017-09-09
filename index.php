2b112a2ca7ed0c841011baf60f0bfd89d9db0cf3
stroke-one <stroke-one@users.noreply.github.com>
2017-09-09 07:52:20 -0700

    switched to PDO

--- index.php
+++ index.php
@@ -13,23 +13,18 @@
         <?php
             if (!empty($_POST['name'])){
                 echo $_POST['name'];
-                $con = mysql_connect('localhost', 'ben', '...')
-                        or die('Unable to connect: ' . mysql_error());
-                mysql_select_db('phptest') or die ('unable to select db');
+                $con = new PDO('mysql:host=localhost;dbname=phptest', 'ben', '...');                   
                 $sql_insert = 'insert into names (name) VALUES (' . $_POST['name'] . ')';
-                if (($con->query($sql) === TRUE){
-                    $sql = 'select name from names';
-                    $result = $con->query($sql);
-                    if ($result->num_rows > 0){
-                        while($row = $result->fetch_assoc())){
-                            echo("<p>" . $row["name"]) . "</p>"
-                        }
-                    }
-                    
-                     
+                $cur = $con->prepare($sql_insert);
+                $cur->execute();
+                            
+                $sql = "select name from names";
+                $cur = $con->prepare($sql);
+                $cur->execute();
+                $result = $cur->fetchAll();
+                print_r($result);
+
                 }
-                
-            }
         ?>
     </center>        
     </body>
